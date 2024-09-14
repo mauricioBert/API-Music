f0dd2885c091e8f08b4006afa053c00f31bccf3b
Bertoldo <128431515+mauricioBert@users.noreply.github.com>
2024-09-13 22:40:58 -0300

    Delete API-MusicDiego directory

--- API-MusicDiego/routes/musicRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const musicRoutes = express.Router()
-import musicControllers from "../controllers/musicControllers.js";
-
-musicRoutes.get("/music", musicControllers.getAllMusic);
-
-export default musicRoutes
